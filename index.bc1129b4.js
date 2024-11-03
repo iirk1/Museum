2884ec86cff8a45174c29c7eebc668423df74703
Iryna <irakogut23190@gmail.com>
2024-11-03 17:07:00 +0200

    make build

---
+++ index.bc1129b4.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",breakpoints:{320:{slidesPerView:1},640:{slidesPerView:2},920:{slidesPerView:3}},spaceBetween:30,loop:!1,pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.bc1129b4.js.map
